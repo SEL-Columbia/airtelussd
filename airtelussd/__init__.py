8d4e79be9e60cd3bbcea0404f6c3f7134b695869
Ivan Willig <iwillig@gmail.com>
2011-12-06 16:22:08 -0500

    BLah.

--- airtelussd/__init__.py
+++ airtelussd/__init__.py
@@ -24,7 +24,7 @@ def index_menu(request, session_id, user_input):
     """
     Function to render the main SharedSolar USSD index.
     """
-    return Response(templates.index.render(menu=options))
+    return Response(templates.index.render(menu=options), headerslist=[''])
 
 
 def check_balance(request, session_id, user_input):
@@ -55,7 +55,7 @@ def index(request):
         # show the index menu if this is a new request
         if request_new_p == True:
             return index_menu(request, session_id, user_input)
-        if request_new_p == False and user_input == 1:
+        if request_new_p == False and user_input == 'A':
             check_balance()
         return error(user_input)
     else:
